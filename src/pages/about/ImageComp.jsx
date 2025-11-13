bcf4f2aae721c298a09a39515f27100f4c1d5059
Your Name <your.email@example.com>
2025-11-13 12:07:34 +0900

    251113_1200

--- src/pages/about/ImageComp.jsx
+++ src/pages/about/ImageComp.jsx
@@ -52,6 +52,7 @@ function ImageComp() {
     }
 
     const bucket = 'images';
+    const tabel = 'image_upload';
     const filepath = `${Date.now()}_${selectFile.name}`;
 
     //파일이름을 uuid + "_" + selectfile.name -> 1757035671640..._han.png
@@ -61,20 +62,48 @@ function ImageComp() {
     const { error } = await supabase.storage
       .from(bucket)
       .upload(filepath, selectFile);
+    // console.log(data.publicUrl);
 
     if (error) {
       setMessage('업로드실패 : ' + error.message);
       return;
     } else {
-      toast('업로드 완료되었습니다.');
+      //
+      //
 
       //파일경로전달받음
-      const { data } = supabase.storage.from(bucket).getPublicUrl(filepath);
-      console.log(data.publicUrl);
+      const { data, error: urlErr } = await supabase.storage
+        .from(bucket)
+        .getPublicUrl(filepath);
+      // console.log(data.publicUrl);
       setUploadUrl(data.publicUrl);
-      setFileName('+');
+
+      if (!urlErr) {
+        const { error: insertErr } = await supabase.from(tabel).insert({
+          fileName: fileName,
+          fileurl: uploadUrl,
+        });
+
+        if (!insertErr) {
+          toast('업로드 완료되었습니다.');
+          setFileName('+');
+        }
+      }
+      //
+      //
+      //
+      //
+      //
     }
   };
+
+  const clearPreview = () => {
+    setPreview('');
+    setSelectFile(null);
+    setFileName('+');
+    fileInputRef.current.value = '';
+  };
+
   return (
     <div>
       <h3>이미지업로드</h3>
@@ -102,27 +131,46 @@ function ImageComp() {
               ref={fileInputRef}
             />
           </div>
-
-          <div className="mb-3 position-reletive bg-info">
-            {preview && (
-              <>
-                {' '}
+          {preview && (
+            <>
+              <div
+                className="mb-3 position-relative bg-info"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '10px',
+                  overflow: 'hidden',
+                }}
+              >
+                <div
+                  className="bg-white btn-sm position-absolute"
+                  style={{
+                    width: '20px',
+                    height: '20px',
+                    borderRadius: '10px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '10px',
+                    lineHeight: '10px',
+                    right: '5px',
+                    top: '5px',
+                  }}
+                  onClick={clearPreview}
+                >
+                  X
+                </div>
                 <img
                   src={preview}
-                  className="shadow"
                   style={{
                     width: '100px',
                     height: '100px',
                     objectFit: 'cover',
-                    borderRadius: '10px',
                   }}
                 />
-                <button className="btn btn-primary btn-sm position-absolute right-0">
-                  삭제
-                </button>
-              </>
-            )}
-          </div>
+              </div>
+            </>
+          )}
 
           <button className="btn btn-primary">파일업로드</button>
           <div>{message && <p className="text-danger mt-2">{message}</p>}</div>
